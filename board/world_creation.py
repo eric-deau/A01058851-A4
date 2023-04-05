07a00a64179e95394cbdac47382219eac82f7db9
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-05 09:52:37 -0700

    Implement add_bosses function

--- board/world_creation.py
+++ board/world_creation.py
@@ -1,4 +1,5 @@
 import random
+from board import FLOORS
 
 
 def make_board(rows, columns, floors):
@@ -14,6 +15,9 @@ def make_board(rows, columns, floors):
                             "last hour and have been thoroughly looted except for their clothes."]
     board = {(row, column, floor): list_of_descriptions[random.randint(0, len(list_of_descriptions)-1)]
              for row in range(rows) for column in range(columns) for floor in range(floors)}
+    # board = {(row, column, floor): 'Test'
+    #          for row in range(rows) for column in range(columns) for floor in range(floors)}
+    add_bosses(board, rows, columns, floors)
     first_floor_descriptions(board)
     second_floor_descriptions(board)
     third_floor_descriptions(board)
@@ -24,20 +28,25 @@ def first_floor_descriptions(board):
     board[(0, 0, 0)] = "Looks like this is my starting point."
     board[(3, 4, 0)] = "You feel an evil presence watching you.."
     board[(4, 3, 0)] = "The air is getting colder around you.."
-    board[(4, 4, 0)] = "BOSS HERE"
+    # board[(4, 4, 0)] = "BOSS HERE"
 
 
 def second_floor_descriptions(board):
     board[(1, 0, 1)] = "The starting point from the second floor. The door to the west leads back down."
     board[(3, 4, 1)] = "There is noise coming from the door to the east.."
     board[(4, 3, 1)] = "There is noise coming from the door to the south.."
-    board[(4, 4, 1)] = "BOSS HERE"
+    # board[(4, 4, 1)] = "BOSS HERE"
 
 
 def third_floor_descriptions(board):
     board[(3, 4, 2)] = "You feel an ominous presence coming from the door to the east.."
     board[(4, 3, 2)] = "You feel an ominous presence coming from the door to the south.."
-    board[(4, 4, 2)] = "BOSS HERE"
+    # board[(4, 4, 2)] = "BOSS HERE"
+
+
+def add_bosses(board, rows, columns, floors):
+    for floor in range(floors):
+        board[(rows-1, columns-1, floor)] = "BOSS HERE"
 
 
 def edit_floor_descriptions(character, board):
@@ -48,7 +57,7 @@ def edit_floor_descriptions(character, board):
 
 def main():
     print("Hello World")
-    make_board(5, 5, 3)
+    print(make_board(5, 5, 3))
 
 
 if __name__ == "__main__":
