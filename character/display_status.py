adf7d7336a39af1afa419771f2062a4ef607a786
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-05 22:42:15 -0700

    Add docstrings in display_status.py

--- character/display_status.py
+++ character/display_status.py
@@ -1,17 +1,34 @@
 def display_status(character):
+    """
+    Display the characteristics of a character.
+
+    :param character: a dictionary
+    :precondition: character must be a dictionary containing 'Name', 'Class', 'HP', 'MP', 'Level', 'EXP' and
+                    'Affliction'
+    :postcondition: prints the relevant characteristics of a character
+
+    >>> test_char_one = {'Name': 'RAKSHASA', 'Class': 'Mage', 'Attack': 50, 'Spell': 'Doomsday', 'X-coord': 0, \
+                        'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1, 'Turn': False}
+    >>> display_status(test_char_one)
+    Here is your status for RAKSHASA
+    Name: RAKSHASA
+    Class: Mage
+    HP: 100
+    MP: 100
+    EXP: 0
+    Level: 1
+    """
+    attributes = ('Name', 'Class', 'HP', 'MP', 'EXP', 'Level', 'Affliction')
     print(f"Here is your status for {character['Name']}")
     for key, value in character.items():
-        print(f"{key}: {value}")
+        if key in attributes:
+            print(f"{key}: {value}")
 
 
 def main():
     """
-
-    :return:
+    Drive the program.
     """
-    character = {'Name': 'RAKSHASA', 'class': 'Mage', 'Attack': 50, 'Spell': 'Doomsday', 'X-coord': 0, 'Y-coord': 0,
-                 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1, 'Turn': False}
-    display_status(character)
 
 
 if __name__ == "__main__":
