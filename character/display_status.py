9c89496722df9cc53971f4fcc92bdc723cf60df4
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-08 01:14:48 -0700

    Add test_display_status.py to test directory

--- character/display_status.py
+++ character/display_status.py
@@ -9,17 +9,22 @@ def display_status(character):
     :precondition: character must be a dictionary containing 'Name', 'Class', 'HP', 'MP', 'Level', 'EXP' and
                     'Affliction'
     :postcondition: prints the relevant characteristics of a character
+    :raises: TypeError: if character is not a dictionary
     """
-    status_check = input("Would you like to see your status? Type 'Y' to check, else input anything to proceed.")
-    if status_check.lower() == "y":
-        attributes = ('Name', 'Class', 'HP', 'MP', 'EXP', 'Level', 'Affliction')
-        print(f"Here is your status for {character['Name']}")
-        for key, value in character.items():
-            if key in attributes:
-                print(f"{key}: {value}")
-                time.sleep(1)
+    if type(character) is not dict:
+        raise TypeError("Must pass a dictionary as an argument.")
     else:
-        return
+        status_check = input("Would you like to see your status? Type 'Y' to check,"
+                             " else type anything else to proceed.")
+        if status_check.lower() == "y":
+            attributes = ('Name', 'Class', 'HP', 'MP', 'EXP', 'Level', 'Affliction')
+            print(f"Here is your status for {character['Name']}")
+            for key, value in character.items():
+                if key in attributes:
+                    print(f"{key}: {value}")
+                    time.sleep(1)
+        else:
+            return
 
 
 def main():
---
+++ tests/test_display_status.py
@@ -0,0 +1,30 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch
+from character.display_status import display_status
+
+
+class TestDisplayStatus(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=["Y"])
+    def test_display_status_user_displays_status(self, _, mock_output):
+        test_char = {'Affliction': None, 'Attack': 30, 'Class': 'Mage', 'EXP': 0, 'HP': 80, 'Level': 1, 'MP': 150,
+                     'MP Cost': None, 'Name': 'Bobby B', 'Spell': 'Doomsday', 'Turn': False, 'X-coord': 0, 'Y-coord': 0,
+                     'Z-coord': 0}
+        expected = "Here is your status for Bobby B\nAffliction: None\nClass: Mage\nEXP: 0\n" \
+                   "HP: 80\nLevel: 1\nMP: 150\nName: Bobby B\n"
+        display_status(test_char)
+        actual = mock_output.getvalue()
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', side_effect=["No"])
+    def test_display_status_no_display(self, _):
+        test_char = {'Affliction': None, 'Attack': 30, 'Class': 'Mage', 'EXP': 0, 'HP': 80, 'Level': 1, 'MP': 150,
+                     'MP Cost': None, 'Name': 'Bobby B', 'Spell': 'Doomsday', 'Turn': False, 'X-coord': 0, 'Y-coord': 0,
+                     'Z-coord': 0}
+        expected = None
+        self.assertEqual(expected, display_status(test_char))
+
+    def test_display_status_invalid_data_type(self):
+        with self.assertRaises(TypeError):
+            display_status("Not a dictionary.")
