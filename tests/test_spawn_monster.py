4b1e66d55856615fd59298d3907d03d93422e8be
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-08 13:51:48 -0700

    Add additional test to test_spawn_monster.py

--- tests/test_spawn_monster.py
+++ tests/test_spawn_monster.py
@@ -1,5 +1,4 @@
 import io
-import json
 from unittest import TestCase
 from unittest.mock import patch
 from combat.encounters import spawn_monster
@@ -10,7 +9,17 @@ class TestSpawnMonster(TestCase):
     @patch('builtins.open', return_value=io.StringIO('[{"Name": "EYE OF CTHULHU", "HP": 30, "ATK": 25,'
                                                      ' "Affliction": null, "Turn": false, "EXP": 100}]'))
     @patch('random.randint', side_effect=[0, 50])
-    def test_spawn_monster(self, _, __):
+    def test_spawn_monster_length_of_one(self, _, __):
         test_file_content = spawn_monster()
         expected = {"Name": "EYE OF CTHULHU", "HP": 50, "ATK": 25, "Affliction": None, "Turn": False, "EXP": 100}
+        self.assertEqual(expected, test_file_content)
+
+    @patch('builtins.open', return_value=io.StringIO('[{"Name": "EYE OF CTHULHU", "HP": 30, "ATK": 25,'
+                                                     ' "Affliction": null, "Turn": false, "EXP": 100},'
+                                                     ' {"Name": "Banana", "HP": 30, "ATK": 25,'
+                                                     ' "Affliction": null, "Turn": false, "EXP": 100}]'))
+    @patch('random.randint', side_effect=[1, 40])
+    def test_spawn_monster_length_of_two(self, _, __):
+        test_file_content = spawn_monster()
+        expected = {"Name": "Banana", "HP": 40, "ATK": 25, "Affliction": None, "Turn": False, "EXP": 100}
         self.assertEqual(expected, test_file_content)
