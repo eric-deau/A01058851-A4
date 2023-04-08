ebdd128dd78711085d3ebf1f92160110f05f5d33
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-08 00:39:46 -0700

    Add test_thief_stats.py to test directory

---
+++ tests/test_thief_stats.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from character.character_creation import thief_stats
+
+
+class TestThiefStats(TestCase):
+    def test_thief_stats(self):
+        test_char_three = {'Name': 'RAKSHASA', 'Class': 'Thief', 'Attack': None, 'Spell': 'Stab', 'MP Cost': None,
+                           'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1,
+                           'Turn': False, 'Affliction': None}
+        thief_stats(test_char_three)
+        expected = {'Name': 'RAKSHASA', 'Class': 'Thief', 'Attack': 40, 'Spell': 'Stab', 'MP Cost': None,
+                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 90, 'MP': 100, 'EXP': 0, 'Level': 1,
+                    'Turn': False, 'Affliction': None}
+        self.assertEqual(expected, test_char_three)
+
+    def test_warrior_stats_incorrect_type(self):
+        with self.assertRaises(TypeError):
+            thief_stats("Not a dictionary")
