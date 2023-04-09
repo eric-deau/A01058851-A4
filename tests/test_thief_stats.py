a3e860ef639e253f2dadfbb55ba38a1f92796ae3
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-09 00:16:49 -0700

    Edit testing in test_thief_stats.py

--- tests/test_thief_stats.py
+++ tests/test_thief_stats.py
@@ -8,8 +8,8 @@ class TestThiefStats(TestCase):
                            'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1,
                            'Turn': False, 'Affliction': None}
         thief_stats(test_char_three)
-        expected = {'Name': 'RAKSHASA', 'Class': 'Thief', 'Attack': 40, 'Spell': 'Stab', 'MP Cost': None,
-                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 90, 'MP': 100, 'EXP': 0, 'Level': 1,
+        expected = {'Name': 'RAKSHASA', 'Class': 'Thief', 'Attack': 60, 'Spell': 'Stab', 'MP Cost': None,
+                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1,
                     'Turn': False, 'Affliction': None}
         self.assertEqual(expected, test_char_three)
 
