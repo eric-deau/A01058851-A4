c39dc97065f827f19fb7adaf368994b1fe7353d9
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-09 00:00:54 -0700

    Edit test in test_character_attack.py

--- tests/test_character_attack.py
+++ tests/test_character_attack.py
@@ -6,7 +6,7 @@ from combat.game_combat import character_attack
 class TestCharacterAttack(TestCase):
     @patch('builtins.input', side_effect=['1'])
     def test_character_attack_regular_attack(self, _):
-        test_char_one = {'Name': 'RAKSHASA', 'Class': 'Mage', 'Attack': 10, 'Spell': 'Doomsday', 'X-coord': 0,
+        test_char_one = {'Name': 'Bob', 'Class': 'Mage', 'Attack': 10, 'Spell': 'Doomsday', 'X-coord': 0,
                          'Y-coord': 0, 'Z-coord': 0, 'HP': 300, 'MP': 200, 'EXP': 222, 'Level': 2, 'Turn': False,
                          'Affliction': None}
         test_creep_one = {'Name': 'EYE OF CTHULHU', 'HP': 20, 'ATK': 25, 'Affliction': None, 'Turn': False,
@@ -23,7 +23,7 @@ class TestCharacterAttack(TestCase):
         test_creep_one = {'Name': 'EYE OF CTHULHU', 'HP': 20, 'ATK': 25, 'Affliction': None, 'Turn': False,
                           'EXP': 100}
         character_attack(test_char_one, test_creep_one)
-        expected = {'Name': 'EYE OF CTHULHU', 'HP': -32.0, 'ATK': 25, 'Affliction': 'Burn', 'Turn': True, 'EXP': 100}
+        expected = {'Name': 'EYE OF CTHULHU', 'HP': -62.0, 'ATK': 25, 'Affliction': 'Burn', 'Turn': True, 'EXP': 100}
         self.assertEqual(expected, test_creep_one)
 
     @patch('builtins.input', side_effect=['3'])
