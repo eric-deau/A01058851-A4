e865c0f6b84153904472e732a4d08f87c9242653
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-09 00:06:29 -0700

    Edit testing in test_display_status.py

--- tests/test_display_status.py
+++ tests/test_display_status.py
@@ -8,10 +8,10 @@ class TestDisplayStatus(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=["Y"])
     def test_display_status_user_displays_status(self, _, mock_output):
-        test_char = {'Affliction': None, 'Attack': 30, 'Class': 'Mage', 'EXP': 0, 'HP': 80, 'Level': 1, 'MP': 150,
+        test_char = {'Affliction': None, 'Attack': 30, 'Class': 'Warrior', 'EXP': 0, 'HP': 80, 'Level': 1, 'MP': 150,
                      'MP Cost': None, 'Name': 'Bobby B', 'Spell': 'Doomsday', 'Turn': False, 'X-coord': 0, 'Y-coord': 0,
                      'Z-coord': 0}
-        expected = "Here is your status for Bobby B\nAffliction: None\nClass: Mage\nEXP: 0\n" \
+        expected = "Here is your status for Bobby B\nAffliction: None\nClass: Warrior\nEXP: 0\n" \
                    "HP: 80\nLevel: 1\nMP: 150\nName: Bobby B\n"
         display_status(test_char)
         actual = mock_output.getvalue()
@@ -19,7 +19,7 @@ class TestDisplayStatus(TestCase):
 
     @patch('builtins.input', side_effect=["No"])
     def test_display_status_no_display(self, _):
-        test_char = {'Affliction': None, 'Attack': 30, 'Class': 'Mage', 'EXP': 0, 'HP': 80, 'Level': 1, 'MP': 150,
+        test_char = {'Affliction': None, 'Attack': 30, 'Class': 'Thief', 'EXP': 0, 'HP': 80, 'Level': 1, 'MP': 150,
                      'MP Cost': None, 'Name': 'Bobby B', 'Spell': 'Doomsday', 'Turn': False, 'X-coord': 0, 'Y-coord': 0,
                      'Z-coord': 0}
         expected = None
