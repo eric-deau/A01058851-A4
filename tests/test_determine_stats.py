c684662dd7a7d3cc0b4bd987cc5df12deb1498fc
ERIC DEAU <=edeau@my.bcit.ca>
2023-04-09 00:05:36 -0700

    Edit testing in test_determine_stats.py

--- tests/test_determine_stats.py
+++ tests/test_determine_stats.py
@@ -4,32 +4,32 @@ from character.character_creation import determine_stats
 
 class TestDetermineStats(TestCase):
     def test_determine_stats_mage(self):
-        test_char_one = {'Name': 'RAKSHASA', 'Class': 'Mage', 'Attack': None, 'Spell': 'Doomsday', 'MP Cost': None,
+        test_char_one = {'Name': 'RAK', 'Class': 'Mage', 'Attack': None, 'Spell': 'Doomsday', 'MP Cost': None,
                          'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1,
                          'Turn': False, 'Affliction': None}
         determine_stats(test_char_one)
-        expected = {'Name': 'RAKSHASA', 'Class': 'Mage', 'Attack': 30, 'Spell': 'Doomsday', 'MP Cost': None,
-                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 80, 'MP': 150, 'EXP': 0, 'Level': 1,
+        expected = {'Name': 'RAK', 'Class': 'Mage', 'Attack': 35, 'Spell': 'Doomsday', 'MP Cost': None,
+                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 90, 'MP': 150, 'EXP': 0, 'Level': 1,
                     'Turn': False, 'Affliction': None}
         self.assertEqual(expected, test_char_one)
 
     def test_determine_stats_warrior(self):
-        test_char_two = {'Name': 'RAKSHASA', 'Class': 'Warrior', 'Attack': None, 'Spell': 'Earthquake Chain',
+        test_char_two = {'Name': 'Bob', 'Class': 'Warrior', 'Attack': None, 'Spell': 'Earthquake Chain',
                          'MP Cost': None, 'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0,
                          'Level': 1, 'Turn': False, 'Affliction': None}
         determine_stats(test_char_two)
-        expected = {'Name': 'RAKSHASA', 'Class': 'Warrior', 'Attack': 50, 'Spell': 'Earthquake Chain',
+        expected = {'Name': 'Bob', 'Class': 'Warrior', 'Attack': 50, 'Spell': 'Earthquake Chain',
                     'MP Cost': None, 'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 110, 'MP': 50, 'EXP': 0,
                     'Level': 1, 'Turn': False, 'Affliction': None}
         self.assertEqual(expected, test_char_two)
 
     def test_determine_stats_thief(self):
-        test_char_three = {'Name': 'RAKSHASA', 'Class': 'Thief', 'Attack': None, 'Spell': 'Stab', 'MP Cost': None,
+        test_char_three = {'Name': 'Teef', 'Class': 'Thief', 'Attack': None, 'Spell': 'Stab', 'MP Cost': None,
                            'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1,
                            'Turn': False, 'Affliction': None}
         determine_stats(test_char_three)
-        expected = {'Name': 'RAKSHASA', 'Class': 'Thief', 'Attack': 40, 'Spell': 'Stab', 'MP Cost': None,
-                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 90, 'MP': 100, 'EXP': 0, 'Level': 1,
+        expected = {'Name': 'Teef', 'Class': 'Thief', 'Attack': 60, 'Spell': 'Stab', 'MP Cost': None,
+                    'X-coord': 0, 'Y-coord': 0, 'Z-coord': 0, 'HP': 100, 'MP': 100, 'EXP': 0, 'Level': 1,
                     'Turn': False, 'Affliction': None}
         self.assertEqual(expected, test_char_three)
 
